10c4c07ce746ff507a8fbba4bcb374d7f3f8faaf
NotKyoyo <hey.kyoyo@gmail.com>
2021-07-04 18:39:43 +0530

    Remove express static serve

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 4000; // PORT for the Express server to run on
-app.set('trust proxy', 1);
-
-app.use('/', express.static('docs-dist'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
